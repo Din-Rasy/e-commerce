ab663b5b7a6e6179de26a79a08b6e5fbc1e95adc
Din-Rasy <rasy.gic.itc@gmail.com>
2026-01-23 00:19:34 +0700

    completed TP03

--- src/App.vue
+++ src/App.vue
@@ -99,93 +99,7 @@ export default {
       categories: [],
       promotions: [],
 
-      products: [
-        {
-          name: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
-          category: "Fruits",
-          weight: "500g",
-          price: 2.51,
-          oldPrice: 2.80,
-          image: "/product/Mango.jpg",
-          badge: "-17%",
-        },
-        {
-          name: "Foster Farms Takeout Crispy Classic Buffalo Wings",
-          category: "Vegetables",
-          weight: "500g",
-          price: 3.00,
-          oldPrice: 3.50,
-          image: "/product/Bell peppers.png",
-          badge: "Hot",
-        },
-        {
-          name: "All Natural Italian-Style Chicken Meatballs",
-          category: "Vegetables",
-          weight: "500g",
-          price: 1.49,
-          oldPrice: 1.89,
-          image: "/product/Corn.png",
-          badge: "Sale",
-        },
-        {
-          name: "Blue Diamond Almonds Lightly Salted Vegetables",
-          category: "Fruits",
-          weight: "500g",
-          price: 2.20,
-          oldPrice: 2.60,
-          image: "/product/Green grapes.png",
-        },
-        {
-          name: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
-          category: "Fruits",
-          weight: "500g",
-          price: 2.80,
-          oldPrice: 3.10,
-          image: "/product/Mandarins.png",
-          badge: "-10%",
-        },
-        {
-          name: "Encore Seafoods Stuffed Alaskan Salmon",
-          category: "Meats",
-          weight: "500g",
-          price: 5.80,
-          oldPrice: 6.30,
-          image: "/product/Roast beef.png",
-        },
-        {
-          name: "Gorton’s Beer Battered Fish Fillets with soft paper",
-          category: "Meats",
-          weight: "500g",
-          price: 4.20,
-          oldPrice: 4.60,
-          image: "/product/Bacon strips.png",
-        },
-        {
-          name: "Canada Dry Ginger Ale – 2 L Bottle - 200ml - 400g",
-          category: "Meats",
-          weight: "500g",
-          price: 3.60,
-          oldPrice: 4.10,
-          image: "/product/Cheese.png",
-        },
-        {
-          name: "Chobani Complete Vanilla Greek Yogurt",
-          category: "Meats",
-          weight: "500g",
-          price: 4.99,
-          oldPrice: 5.50,
-          image: "/product/Raw pork.png",
-        },
-        {
-          name: "Haagen-Dazs Caramel Cone Ice Cream Ketchup",
-          category: "Vegetables",
-          weight: "500g",
-          price: 1.90,
-          oldPrice: 2.30,
-          image: "/product/Red beetroot.png",
-          badge: "Hot",
-        },
-      ]
+      products: [],
 
     };
   },
@@ -214,11 +128,24 @@ export default {
         console.error(error);
       }
     },
+
+    async fetchProducts() {
+      try {
+        const res = await axios.get("http://localhost:3000/api/products");
+        this.products = res.data.map((item) => ({
+          ...item,
+          image: item.image ? "http://localhost:3000/" + item.image.replace(/\\/g, "/") : "/product/default.jpg",
+        }));
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
 
   mounted() {
     this.fetchCategories();
     this.fetchPromotions();
+    this.fetchProducts();
   },
 };
 </script>
